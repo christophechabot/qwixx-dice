ecff482dfc1c75733e204411d57ee6780509ca50
Christophe Chabot <christophe.chabot.wd@gmail.com>
2018-12-22 18:16:45 +0000

    First commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,198 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div id="left-column">
+      <div id="left-column-content">
+        <Die :value="values[0]" color="neutral"/>
+        <Die :value="values[2]" color="red"/>
+        <Die :value="values[3]" color="blue"/>
+        <Die :value="values[1]" color="neutral"/>
+        <Die :value="values[4]" color="green"/>
+        <Die :value="values[5]" color="yellow"/>
+        <button @click="roll">Roll</button>
+      </div>
+    </div>
+    <div id="right-column">
+      <div id="right-column-content">
+        <div class="right-column-row">
+          <Die :value="values[0]" color="neutral"/>
+          <span>+</span>
+          <Die :value="values[1]" color="neutral"/>
+          <span>=</span>
+          <span class="total neutral">{{ values[0] + values[1] }}</span>
+        </div>
+        <div class="right-column-separator"></div>
+        <div class="right-column-row">
+          <span>
+            <Die :value="values[0]" color="neutral"/>
+            <span>+</span>
+            <Die :value="values[2]" color="red"/>
+            <span>=</span>
+            <span class="total red">{{ values[0] + values[2] }}</span>
+          </span>
+          <span v-if="values[0] !== values[1]">
+            <Die :value="values[1]" color="neutral"/>
+            <span>+</span>
+            <Die :value="values[2]" color="red"/>
+            <span>=</span>
+            <span class="total red">{{ values[1] + values[2] }}</span>
+          </span>
+        </div>
+        <div class="right-column-row">
+          <span>
+            <Die :value="values[0]" color="neutral"/>
+            <span>+</span>
+            <Die :value="values[3]" color="blue"/>
+            <span>=</span>
+            <span class="total blue">{{ values[0] + values[3] }}</span>
+          </span>
+          <span v-if="values[0] !== values[1]">
+            <Die :value="values[1]" color="neutral"/>
+            <span>+</span>
+            <Die :value="values[3]" color="blue"/>
+            <span>=</span>
+            <span class="total blue">{{ values[1] + values[3] }}</span>
+          </span>
+        </div>
+        <div class="right-column-row">
+          <span>
+            <Die :value="values[0]" color="neutral"/>
+            <span>+</span>
+            <Die :value="values[4]" color="green"/>
+            <span>=</span>
+            <span class="total green">{{ values[0] + values[4] }}</span>
+          </span>
+          <span v-if="values[0] !== values[1]">
+            <Die :value="values[1]" color="neutral"/>
+            <span>+</span>
+            <Die :value="values[4]" color="green"/>
+            <span>=</span>
+            <span class="total green">{{ values[1] + values[4] }}</span>
+          </span>
+        </div>
+        <div class="right-column-row">
+          <span>
+            <Die :value="values[0]" color="neutral"/>
+            <span>+</span>
+            <Die :value="values[5]" color="yellow"/>
+            <span>=</span>
+            <span class="total yellow">{{ values[0] + values[5] }}</span>
+          </span>
+          <span v-if="values[0] !== values[1]">
+            <Die :value="values[1]" color="neutral"/>
+            <span>+</span>
+            <Die :value="values[5]" color="yellow"/>
+            <span>=</span>
+            <span class="total yellow">{{ values[1] + values[5] }}</span>
+          </span>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Die from './components/Die.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Die
+  },
+  data: function() {
+    return {
+      values: [1, 2, 3, 4, 5, 6]
+    };
+  },
+  methods: {
+    roll() {
+      this.values = [1, 2, 3, 4, 5, 6]
+        .map(() => Math.floor(Math.random() * 6) + 1);
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body {
+  margin: 0;
+  padding: 0;
 }
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    color: #2c3e50;
+  }
+  #left-column,
+  #right-column {
+    box-sizing: border-box;
+    display: inline-block;
+    vertical-align: middle;
+    position: relative;
+    width: 50%;
+    height: 100vh;
+    padding: 4px;
+    text-align: center;
+  }
+  #left-column-content,
+  #right-column-content {
+    position: absolute;
+    width: 100%;
+    top: 50%;
+    left: 0;
+    transform: translate(0, -50%);
+    text-align: center;
+  }
+  #left-column {
+    border-right: 2px solid #2c3e50;
+  }
+  #left-column .die {
+    box-sizing: border-box;
+    width: 33%;
+    height: auto;
+    padding: 8px;
+  }
+  #left-column button {
+    background: none;
+    border: 2px solid #2c3e50;
+    border-radius: 4px;
+    padding: 9px 16px;
+    margin-top: 32px;
+    font-size: 24px;
+  }
+  #right-column .die {
+    width: 36px;
+    height: 36px;
+    padding: 8px;
+  }
+  .right-column-row {
+    padding: 0 8px;
+  }
+  .right-column-row span {
+    display: inline-block;
+    vertical-align: middle;
+    font-size: 20px;
+    font-weight: bold;
+  }
+  .right-column-row .total {
+    padding: 0 8px;
+    font-size: 24px;
+  }
+  .right-column-row .total.red {
+    color: #d73f4c;
+  }
+  .right-column-row .total.blue {
+    color: #394a8b;
+  }
+  .right-column-row .total.green {
+    color: #419f5b;
+  }
+  .right-column-row .total.yellow {
+    color: #d8a352;
+  }
+  .right-column-separator {
+    margin: 16px 32px;
+    height: 1px;
+    border-bottom: 2px solid #2c3e50;
+  }
 </style>
---
+++ src/components/Die.vue
@@ -0,0 +1,127 @@
+<template>
+  <span :class="color">
+    <svg
+      v-if="value === 1"
+      class="die"
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 512 512">
+      <path d="M0 0h512v512H0z" :fill="primaryColor" fill-opacity="1">
+      </path>
+      <g class="" transform="translate(0,0)" style="touch-action: none;">
+        <path d="M302.87 255.5a47.37 47.37 0 1 1-47.37-47.37 47.37 47.37 0 0 1 47.37 47.37zM484.5 428.02a56.48 56.48 0 0 1-56.48 56.48h-344a56.48 56.48 0 0 1-56.52-56.48v-344A56.48 56.48 0 0 1 83.98 27.5h344a56.48 56.48 0 0 1 56.52 56.48zm-20-344a36.48 36.48 0 0 0-36.48-36.52h-344A36.48 36.48 0 0 0 47.5 83.98v344a36.48 36.48 0 0 0 36.48 36.52h344a36.48 36.48 0 0 0 36.52-36.48z" :fill="secondaryColor" fill-opacity="1">
+        </path>
+      </g>
+    </svg>
+
+    <svg
+      v-if="value === 2"
+      class="die"
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 512 512">
+      <path d="M0 0h512v512H0z" :fill="primaryColor" fill-opacity="1">
+      </path>
+      <g class="" transform="translate(0,0)" style="touch-action: none;">
+        <path d="M383 81.68A47.37 47.37 0 1 1 335.58 129 47.37 47.37 0 0 1 383 81.68zM81.67 383A47.37 47.37 0 1 0 129 335.59 47.37 47.37 0 0 0 81.67 383zM428 47.57H84A36.48 36.48 0 0 0 47.57 84v344A36.48 36.48 0 0 0 84 464.43h344A36.48 36.48 0 0 0 464.43 428V84A36.48 36.48 0 0 0 428 47.57m0-20A56.54 56.54 0 0 1 484.43 84v344A56.54 56.54 0 0 1 428 484.43H84A56.54 56.54 0 0 1 27.57 428V84A56.54 56.54 0 0 1 84 27.57z" :fill="secondaryColor" fill-opacity="1">
+        </path>
+      </g>
+    </svg>
+
+    <svg
+      v-if="value === 3"
+      class="die"
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 512 512">
+      <path d="M0 0h512v512H0z" :fill="primaryColor" fill-opacity="1">
+      </path>
+      <g class="" transform="translate(0,0)" style="touch-action: none;">
+        <path d="M302.87 255.5a47.37 47.37 0 1 1-47.37-47.37 47.37 47.37 0 0 1 47.37 47.37zM382.5 81.18a47.37 47.37 0 1 0 47.32 47.32 47.37 47.37 0 0 0-47.32-47.32zm-254 253.91a47.37 47.37 0 1 0 47.41 47.41 47.37 47.37 0 0 0-47.41-47.41zm356 92.94a56.48 56.48 0 0 1-56.48 56.47h-344a56.48 56.48 0 0 1-56.52-56.48v-344A56.48 56.48 0 0 1 83.98 27.5h344a56.48 56.48 0 0 1 56.52 56.48zm-20-344a36.48 36.48 0 0 0-36.48-36.53h-344A36.48 36.48 0 0 0 47.5 83.98v344a36.48 36.48 0 0 0 36.48 36.52h344a36.48 36.48 0 0 0 36.52-36.48z" :fill="secondaryColor" fill-opacity="1">
+        </path>
+      </g>
+    </svg>
+
+    <svg
+      v-if="value === 4"
+      class="die"
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 512 512">
+      <path d="M0 0h512v512H0z" :fill="primaryColor" fill-opacity="1">
+      </path>
+      <g class="" transform="translate(0,0)" style="touch-action: none;">
+        <path d="M175.91 128.5a47.37 47.37 0 1 1-47.41-47.32 47.37 47.37 0 0 1 47.41 47.32zM382.5 81.18a47.37 47.37 0 1 0 47.32 47.32 47.37 47.37 0 0 0-47.32-47.32zm-254 253.91a47.37 47.37 0 1 0 47.41 47.41 47.37 47.37 0 0 0-47.41-47.41zm253.91 0a47.37 47.37 0 1 0 47.41 47.41 47.37 47.37 0 0 0-47.32-47.41zm102 92.93a56.48 56.48 0 0 1-56.39 56.48h-344a56.48 56.48 0 0 1-56.52-56.48v-344A56.48 56.48 0 0 1 83.98 27.5h344a56.48 56.48 0 0 1 56.52 56.48zm-20-344a36.48 36.48 0 0 0-36.39-36.52h-344A36.48 36.48 0 0 0 47.5 83.98v344a36.48 36.48 0 0 0 36.48 36.52h344a36.48 36.48 0 0 0 36.52-36.48z" :fill="secondaryColor" fill-opacity="1">
+        </path>
+      </g>
+    </svg>
+
+    <svg
+      v-if="value === 5"
+      class="die"
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 512 512">
+      <path d="M0 0h512v512H0z" :fill="primaryColor" fill-opacity="1">
+      </path>
+      <g class="" transform="translate(0,0)" style="touch-action: none;">
+        <path d="M302.87 255.5a47.37 47.37 0 1 1-47.37-47.37 47.37 47.37 0 0 1 47.37 47.37zM128.5 81.18a47.37 47.37 0 1 0 47.41 47.32 47.37 47.37 0 0 0-47.41-47.32zm253.91 0a47.37 47.37 0 1 0 47.41 47.32 47.37 47.37 0 0 0-47.32-47.32zM128.5 335.09a47.37 47.37 0 1 0 47.41 47.41 47.37 47.37 0 0 0-47.41-47.41zm253.91 0a47.37 47.37 0 1 0 47.41 47.41 47.37 47.37 0 0 0-47.32-47.41zm102 92.93a56.48 56.48 0 0 1-56.39 56.48h-344a56.48 56.48 0 0 1-56.52-56.48v-344A56.48 56.48 0 0 1 83.98 27.5h344a56.48 56.48 0 0 1 56.52 56.48zm-20-344a36.48 36.48 0 0 0-36.39-36.52h-344A36.48 36.48 0 0 0 47.5 83.98v344a36.48 36.48 0 0 0 36.48 36.52h344a36.48 36.48 0 0 0 36.52-36.48z" :fill="secondaryColor" fill-opacity="1">
+        </path>
+      </g>
+    </svg>
+
+    <svg
+      v-if="value === 6"
+      class="die"
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 512 512">
+      <path d="M0 0h512v512H0z" :fill="primaryColor" fill-opacity="1">
+      </path>
+      <g class="" transform="translate(0,0)" style="touch-action: none;">
+        <path d="M175.91 128.5a47.37 47.37 0 1 1-47.41-47.32 47.37 47.37 0 0 1 47.41 47.32zM382.5 81.18a47.37 47.37 0 1 0 47.32 47.32 47.37 47.37 0 0 0-47.32-47.32zm-254 126.95a47.37 47.37 0 1 0 47.41 47.37 47.37 47.37 0 0 0-47.41-47.37zm253.91 0a47.37 47.37 0 1 0 47.41 47.37 47.37 47.37 0 0 0-47.32-47.37zM128.5 335.09a47.37 47.37 0 1 0 47.41 47.41 47.37 47.37 0 0 0-47.41-47.41zm253.91 0a47.37 47.37 0 1 0 47.41 47.41 47.37 47.37 0 0 0-47.32-47.41zm102 92.93a56.48 56.48 0 0 1-56.39 56.48h-344a56.48 56.48 0 0 1-56.52-56.48v-344A56.48 56.48 0 0 1 83.98 27.5h344a56.48 56.48 0 0 1 56.52 56.48zm-20-344a36.48 36.48 0 0 0-36.39-36.52h-344A36.48 36.48 0 0 0 47.5 83.98v344a36.48 36.48 0 0 0 36.48 36.52h344a36.48 36.48 0 0 0 36.52-36.48z" :fill="secondaryColor" fill-opacity="1">
+        </path>
+      </g>
+    </svg>
+  </span>
+</template>
+
+<script>
+export default {
+  name: 'die',
+  props: {
+    value: Number,
+    color: String
+  },
+  computed: {
+    primaryColor() {
+      switch(this.color) {
+        case 'red':
+          return '#d73f4c';
+        case 'blue':
+          return '#394a8b';
+        case 'green':
+          return '#419f5b';
+        case 'yellow':
+          return '#fee44a';
+        case 'neutral':
+        default:
+          return '#444444';
+      }
+    },
+    secondaryColor() {
+      switch(this.color) {
+        case 'red':
+        case 'green':
+        case 'yellow':
+          return '#444444';
+        case 'neutral':
+        case 'blue':
+        default:
+          return 'white';
+      }
+    }
+  }
+}
+</script>
+
+<style>
+  .die {
+    display: inline-block;
+    vertical-align: middle;
+  }
+</style>
