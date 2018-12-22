ec1ded72d838118b762405448ee0193d13984aa1
Christophe Chabot <christophe.chabot.wd@gmail.com>
2018-12-22 18:23:44 +0000

    Added docs folder

---
+++ docs/precache-manifest.014ce77d567ac7b99195f927684e36ce.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/qwixx-dice/robots.txt"
+  },
+  {
+    "revision": "e6f544a6fd95adcb2aae",
+    "url": "/qwixx-dice/js/chunk-vendors.c1e41029.js"
+  },
+  {
+    "revision": "477e3dc9eb90110a2c62",
+    "url": "/qwixx-dice/js/app.8148a255.js"
+  },
+  {
+    "revision": "15672cafa12f335f7e3c0e54db759a87",
+    "url": "/qwixx-dice/index.html"
+  },
+  {
+    "revision": "477e3dc9eb90110a2c62",
+    "url": "/qwixx-dice/css/app.ab4b8cf5.css"
+  }
+];
